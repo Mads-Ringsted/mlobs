e853a0ae97fa0ce447759335561d031a1e147ff3
Mads-Ringsted <mads@ringsted.com>
2023-01-10 11:04:18 +0100

    test path

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,11 +3,13 @@ from tests import _PATH_DATA
 import torch
 import os
 
-@pytest.mark.skipif(not os.path.exists(_PATH_DATA + '/processed/trainset.pt'), reason="Data files not found")
-@pytest.mark.skipif(not os.path.exists(_PATH_DATA + '/processed/testset.pt'), reason="Data files not found")
+# @pytest.mark.skipif(not os.path.exists(_PATH_DATA + '/processed/trainset.pt'), reason="Data files not found")
+# @pytest.mark.skipif(not os.path.exists(_PATH_DATA + '/processed/testset.pt'), reason="Data files not found")
+@pytest.mark.skipif(not os.path.exists('data' + '/processed/trainset.pt'), reason="Data files not found")
+@pytest.mark.skipif(not os.path.exists('data' + '/processed/testset.pt'), reason="Data files not found")
 def test_data():
-    trainset = torch.load(_PATH_DATA + '/processed/trainset.pt')
-    testset = torch.load(_PATH_DATA + '/processed/testset.pt')
+    trainset = torch.load('data' + '/processed/trainset.pt')
+    testset = torch.load('data' + '/processed/testset.pt')
     assert len(trainset) == 25000, "Trainset have incorrect length"
     assert len(testset) == 5000, "Testst have incorrect length"
     for data_point, _ in trainset:
